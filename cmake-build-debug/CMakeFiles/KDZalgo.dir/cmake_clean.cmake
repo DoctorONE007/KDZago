7e3af127145d680d343fa2de7c184b0b1acb3cef
DoctorONE007 <drone@pswd.ru>
2021-01-21 12:14:25 +0300

    Add project

---
+++ cmake-build-debug/CMakeFiles/KDZalgo.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KDZalgo.dir/main.cpp.obj"
+  "KDZalgo.exe"
+  "KDZalgo.exe.manifest"
+  "KDZalgo.pdb"
+  "libKDZalgo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KDZalgo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
